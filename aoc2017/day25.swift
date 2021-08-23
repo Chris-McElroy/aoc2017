4a5acb65a44ef5cde516074d32c9ea6df4787f69
ChristopherMcElroy <csmcelroy@me.com>
2021-08-23 12:40:42 -0700

    50*

--- aoc2017/day25.swift
+++ aoc2017/day25.swift
@@ -8,7 +8,49 @@
 import Foundation
 
 func day25() {
-	let input = inputLines(25)
+	enum State { case A, B, C, D, E, F }
 	
-	print(input)
+	var state: State = .A
+	var cursor = 0
+	var tape: [Int: Int] = [:]
+	var steps = 0
+	let end = 12386363
+	
+	while steps < end {
+		switch state {
+		case .A:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = v^1
+			cursor += v == 0 ? 1 : -1
+			state = v == 0 ? .B : .E
+		case .B:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = v^1
+			cursor += v == 0 ? -1 : 1
+			state = v == 0 ? .C : .A
+		case .C:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = v^1
+			cursor += v == 0 ? -1 : 1
+			state = v == 0 ? .D : .C
+		case .D:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = v^1
+			cursor += -1
+			state = v == 0 ? .E : .F
+		case .E:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = 1
+			cursor += -1
+			state = v == 0 ? .A : .C
+		case .F:
+			let v = tape[cursor, default: 0]
+			tape[cursor] = 1
+			cursor += v == 0 ? -1 : 1
+			state = v == 0 ? .E : .A
+		}
+		steps += 1
+	}
+	
+	print(tape.values.sum())
 }
